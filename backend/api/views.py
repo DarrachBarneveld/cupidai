adf6a53f5355c0da9e48d90b9186a82033b84c31
Camelia03 <camelia_weber@yahoo.com>
2024-02-17 17:52:33 +0000

    Add Google Places endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from requests import request
-from .serializers import GptSerializer
+from .serializers import GptSerializer, GeolocationSerializer
 from rest_framework import status
 from django.conf import settings
 import json
 
 GPT_MODEL = 'gpt-3.5-turbo'
 GPT_URL = 'https://api.openai.com/v1/chat/completions'
+GOOGLE_PLACES_URL = 'https://places.googleapis.com/v1/places:searchText'
 
 class AskGptView(APIView):
     
@@ -42,3 +43,41 @@ class AskGptView(APIView):
 
        
     
+class GeolocationView(APIView):
+    def post(self, req):
+
+        places_api_key = settings.PLACES_API_KEY
+
+        req_data = GeolocationSerializer(data=req.data)
+
+        # Check if request is valid
+        if req_data.is_valid():
+
+            # Do the request to Google Places API
+            data = {
+                'textQuery': req_data.validated_data.get('text'),
+                'locationBias': {
+                    'circle': {
+                        'center': { 'latitude': req_data.validated_data.get('lat'), 'longitude': req_data.validated_data.get('lng') },
+                        'radius': 1000.0,
+                    },
+                },
+            }
+
+            headers = {
+                "Content-Type": "application/json",
+                "X-Goog-Api-Key": places_api_key,
+                "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.primaryType,places.rating,places.internationalPhoneNumber,places.googleMapsUri",
+            }
+
+            res = request("POST", GOOGLE_PLACES_URL, headers=headers, json=data)
+
+            # Parse the response
+            parsed_json = json.loads(res.text)
+
+            return Response(parsed_json)
+
+        else:  
+             # Return 400 and validation errors
+            return Response(req_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        
