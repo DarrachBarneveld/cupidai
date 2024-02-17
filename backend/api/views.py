3f8ae91c2bdd4f56ec98acea8ca924dfbc1a3427
Camelia03 <camelia_weber@yahoo.com>
2024-02-17 19:59:59 +0000

    Implement API response in chunk for GPT

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,15 +5,24 @@ from .serializers import GptSerializer, GeolocationSerializer
 from rest_framework import status
 from django.conf import settings
 import json
+from openai import OpenAI
+from django.http import StreamingHttpResponse
 
 GPT_MODEL = 'gpt-3.5-turbo'
-GPT_URL = 'https://api.openai.com/v1/chat/completions'
-GOOGLE_PLACES_URL = 'https://places.googleapis.com/v1/places:searchText'
+GOOGLE_PLACES_URL = 'https://places.googleapis.com/v1/places:searchText' 
+
+def stream_response(completion_stream):
+    """ Read chunks from stream """
+    
+    for chunk in completion_stream:
+        content = chunk.choices[0].delta.content
+        if content is None:
+            return
+        yield content
 
 class AskGptView(APIView):
     
     def post(self, req):
-        openai_api_key = settings.OPENAI_API_KEY
 
         req_data = GptSerializer(data=req.data)
 
@@ -21,22 +30,17 @@ class AskGptView(APIView):
         if req_data.is_valid():
 
             # Do the request to open API
-            data = {
-                'model': GPT_MODEL,
-                "messages": [{"role": "user", "content": req_data.validated_data.get('message')}]
-            }
-            res = request("POST", GPT_URL, 
-                          headers={'Authorization': 'Bearer ' + openai_api_key}, json=data)
+            messages = [{"role": "user", "content": req_data.validated_data.get('message')}]
 
-            # Parse GPT response
-            parsed_json = json.loads(res.text)
-            choices = parsed_json['choices']
+            # Create the OpenAi client
+            client = OpenAI()
 
-            # Access GPT message
-            gpt_message = choices[0]['message']['content']
+            # Request GPT stream
+            res = client.chat.completions.create(model=GPT_MODEL, messages=messages, stream=True, max_tokens=500)
 
-            # Return GPT message
-            return Response({'message': gpt_message})
+            # Return a streaming response
+            return StreamingHttpResponse(stream_response(res), content_type='text/plain')
+
         else:
             # Return 400 and validation errors
             return Response(req_data.errors, status=status.HTTP_400_BAD_REQUEST)
