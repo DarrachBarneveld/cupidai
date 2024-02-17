eb9eb337242b35aa116c40a606d57f25287600a8
Camelia03 <camelia_weber@yahoo.com>
2024-02-17 10:12:31 +0000

    Parse and return GPT response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,32 +1,43 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from requests import request
 from .serializers import GptSerializer
-from rest_framework.parsers import JSONParser
 from rest_framework import status
 from django.conf import settings
+import json
+
+GPT_MODEL = 'gpt-3.5-turbo'
+GPT_URL = 'https://api.openai.com/v1/chat/completions'
 
 class AskGptView(APIView):
     
     def post(self, req):
-        print(req.data)
-
         openai_api_key = settings.OPENAI_API_KEY
 
         req_data = GptSerializer(data=req.data)
-        print(req_data)
 
+        # Check if request is valid
         if req_data.is_valid():
+
+            # Do the request to open API
             data = {
-                'model': 'gpt-3.5-turbo',
+                'model': GPT_MODEL,
                 "messages": [{"role": "user", "content": req_data.validated_data.get('message')}]
             }
-            res = request("POST", 'https://api.openai.com/v1/chat/completions', 
+            res = request("POST", GPT_URL, 
                           headers={'Authorization': 'Bearer ' + openai_api_key}, json=data)
 
-            return Response({'message': res.text})
+            # Parse GPT response
+            parsed_json = json.loads(res.text)
+            choices = parsed_json['choices']
+
+            # Access GPT message
+            gpt_message = choices[0]['message']['content']
+
+            # Return GPT message
+            return Response({'message': gpt_message})
         else:
+            # Return 400 and validation errors
             return Response(req_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
        
