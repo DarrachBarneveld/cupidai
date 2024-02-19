fcf337725874e50590de1c5da70cc2fd2b8461fc
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-19 01:25:40 +0000

    Moved into testing

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,61 +1,142 @@
-const drinkForm = document.getElementById("drinkForm");
-const foodForm = document.getElementById("foodForm");
-const activityForm = document.getElementById("activityForm");
-const promptBtn = document.getElementById("promptBtn");
+import ROOT_DATA from "../data/preferences.json";
 
+const choiceContainer = document.getElementById("choiceContainer");
+const refreshBtn = document.getElementById("refreshBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let activity, food, drinks;
+
+ROOT_DATA.forEach((element) => {
+  activity = filterByCategoryId(ROOT_DATA, 1);
+  food = filterByCategoryId(ROOT_DATA, 2);
+  drinks = filterByCategoryId(ROOT_DATA, 3);
+});
+
+let CURRENT_OPTION = 1;
 const selectedDrinks = [];
 const selectedFood = [];
 const selectedActivity = [];
 
-function checkIfAllFormsAreFilled() {
-  if (
-    selectedDrinks.length > 0 &&
-    selectedFood.length > 0 &&
-    selectedActivity.length > 0
-  ) {
-    promptBtn.disabled = false;
+document.addEventListener("DOMContentLoaded", function () {
+  displayChoices(drinks);
+});
+
+function displayChoices(choicesArray) {
+  if (CURRENT_OPTION == 4) {
+    generateRandomPrompt();
+    return;
   }
-}
+  choiceContainer.innerHTML = "";
+  console.log(selectedDrinks);
+  console.log(selectedFood);
+  console.log(selectedActivity);
 
-function setSelectedDrinks(event) {
-  event.preventDefault(); // Prevent form submission
+  const randomItems = getRandomElements(choicesArray, 6);
 
-  const checkboxes = document.querySelectorAll('input[name="drink"]:checked');
-
-  checkboxes.forEach(function (checkbox) {
-    selectedDrinks.push(checkbox.value);
+  randomItems.forEach((item) => {
+    renderChoiceBtnHTML(item);
   });
-
-  console.log("Selected drinks:", selectedDrinks);
-  checkIfAllFormsAreFilled();
 }
 
-function setSelectedFood(event) {
-  event.preventDefault(); // Prevent form submission
-
-  const checkboxes = document.querySelectorAll('input[name="food"]:checked');
-
-  checkboxes.forEach(function (checkbox) {
-    selectedFood.push(checkbox.value);
-  });
-
-  console.log("Selected Food:", selectedFood);
-  checkIfAllFormsAreFilled();
-}
-
-function setSelectedActivity(event) {
-  event.preventDefault(); // Prevent form submission
-
-  const checkboxes = document.querySelectorAll(
-    'input[name="activity"]:checked'
+function renderChoiceBtnHTML(choice) {
+  const containerElement = document.createElement("div");
+  containerElement.classList.add(
+    "col",
+    "px-1",
+    "d-flex",
+    "justify-content-center",
+    "fade-in-bounce"
   );
 
-  checkboxes.forEach(function (checkbox) {
-    selectedActivity.push(checkbox.value);
+  const buttonElement = document.createElement("button");
+  buttonElement.classList.add("choice-btn");
+  buttonElement.textContent = choice.name;
+
+  buttonElement.addEventListener("click", () => {
+    buttonElement.classList.toggle("active");
+
+    const { name } = choice;
+    switch (CURRENT_OPTION) {
+      case 1:
+        if (selectedDrinks.includes(name)) {
+          const index = selectedDrinks.indexOf(name);
+          selectedDrinks.splice(index, 1);
+        } else {
+          selectedDrinks.push(name);
+        }
+        break;
+      case 2:
+        if (selectedFood.includes(name)) {
+          const index = selectedFood.indexOf(name);
+          selectedFood.splice(index, 1);
+        } else {
+          selectedFood.push(name);
+        }
+        break;
+      case 3:
+        if (selectedActivity.includes(name)) {
+          const index = selectedActivity.indexOf(name);
+          selectedActivity.splice(index, 1);
+        } else {
+          selectedActivity.push(name);
+        }
+        break;
+      default:
+        if (selectedDrinks.includes(name)) {
+          const index = selectedDrinks.indexOf(name);
+          selectedDrinks.splice(index, 1);
+        } else {
+          selectedDrinks.push(name);
+        }
+        break;
+    }
   });
 
-  console.log("Selected Activity:", selectedActivity);
-  checkIfAllFormsAreFilled();
+  containerElement.appendChild(buttonElement);
+  choiceContainer.appendChild(containerElement);
+}
+
+function filterByCategoryId(array, categoryId) {
+  return array.filter((element) => element.categoryId === categoryId);
+}
+
+// function to select random elements from array
+function getRandomElements(array, numberOfElements) {
+  // Make a copy of the original array to avoid modifying it
+  const shuffledArray = array.slice();
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  // Return the first 'numberOfElements' elements
+  return shuffledArray.slice(0, numberOfElements);
+}
+
+function refreshCurrentOptions() {
+  choiceContainer.innerHTML = "";
+
+  switch (CURRENT_OPTION) {
+    case 1:
+      selectedDrinks.splice(0, selectedDrinks.length);
+      displayChoices(drinks);
+      break;
+    case 2:
+      selectedFood.splice(0, selectedFood.length);
+
+      displayChoices(food);
+      break;
+    case 3:
+      selectedActivity.splice(0, selectedActivity.length);
+
+      displayChoices(activity);
+      break;
+    default:
+      CURRENT_OPTION = 1;
+      displayChoices(drinks);
+      break;
+  }
 }
 
 function generateRandomPrompt() {
@@ -63,16 +144,22 @@ function generateRandomPrompt() {
   const foodString = selectedFood.join(" ");
   const activityString = selectedActivity.join(" ");
 
-  const interests = drinkString + foodString + activityString;
+  const interests = drinkString + " " + foodString + " " + activityString;
 
-  var url = "results.html?prompt=" + encodeURIComponent(interests);
+  console.log(interests);
+
+  const url = "results.html?prompt=" + encodeURIComponent(interests);
   window.location.href = url;
 }
 
-promptBtn.addEventListener("click", generateRandomPrompt);
+function showNextOptions() {
+  CURRENT_OPTION++;
+  if (CURRENT_OPTION == 2) {
+    displayChoices(food);
+  } else {
+    displayChoices(activity);
+  }
+}
 
-drinkForm.addEventListener("submit", setSelectedDrinks);
-
-foodForm.addEventListener("submit", setSelectedFood);
-
-activityForm.addEventListener("submit", setSelectedActivity);
+refreshBtn.addEventListener("click", refreshCurrentOptions);
+nextBtn.addEventListener("click", showNextOptions);
