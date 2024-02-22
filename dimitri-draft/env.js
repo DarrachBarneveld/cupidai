012d2c8a8e5582ca49d5d123a6c71e9a6bf2c807
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-22 23:47:49 +0000

    Remove css and old files

--- dimitri-draft/env.js
+++
@@ -1,5 +0,0 @@
-// const API_KEY = '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';
-const APIKEY1 = "sk-xpy0IWg";
-const APIKEY2 = "P8oWW2pPKz2hJT3BlbkF";
-const APIKEY3 = "JPBP6CdnUnrAjPNqoxTCb";
-const API_KEY = APIKEY1 + APIKEY2 + APIKEY3;
