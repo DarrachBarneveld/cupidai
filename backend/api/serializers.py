f927197bdb366cbde9d0c5db5a285cb90a1d0eb0
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-29 08:39:32 +0000

    removed requirements

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,8 +5,8 @@ class GptSerializer(serializers.Serializer):
     message = serializers.CharField(required=True)
 
 class GeolocationSerializer(serializers.Serializer):
-    lat = serializers.FloatField(required=True)
-    lng = serializers.FloatField(required=True)
-    drink = serializers.CharField(required=True)
-    food = serializers.CharField(required=True)
-    activity = serializers.CharField(required=True)
+    lat = serializers.FloatField()
+    lng = serializers.FloatField()
+    drink = serializers.CharField()
+    food = serializers.CharField()
+    activity = serializers.CharField()
