c8b1decd8b1af372d4b662b8b1e637a9968a8290
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-05 15:41:53 +0000

    Readded googles maps photo fetch

--- frontend/src/components/PlacesCard.jsx
+++ frontend/src/components/PlacesCard.jsx
@@ -21,8 +21,8 @@ const PlacesCard = memo(
       photoUrl = placeholderPhoto;
     } else {
       const photo = photos[0];
-      // photoUrl = `https://places.googleapis.com/v1/${photo?.name}/media?maxHeightPx=400&maxWidthPx=400&key=${GOOGLE_PLACES_API_KEY}`;
-      photoUrl = placeholderPhoto;
+      photoUrl = `https://places.googleapis.com/v1/${photo?.name}/media?maxHeightPx=400&maxWidthPx=400&key=${GOOGLE_PLACES_API_KEY}`;
+      // photoUrl = placeholderPhoto;
     }
 
     let color = "bg-danger";
